229486b628f44a35ca73b0b263018f8717acdff2
Ahmad Faiz Muharram <faiz64muharram@gmail.com>
2026-06-17 10:44:25 +0700

    Update fitur siswa

--- perpus_project/settings.py
+++ perpus_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yrpvwei)+h1%ejl%3pj0o(0hw0%+ux+&+d)3at-41r$+2e)1k2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['AhmadFaizMuharram.pythonanywhere.com']
 
